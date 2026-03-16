e727980d4438c9e6dc455e99a879c8480282bfec
Maksym Kozlov <maksimpro1337@gmail.com>
2026-03-16 15:36:49 +0100

    feat: added basic variables in script file

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const ratingState = document.getElementById("rating-state");
+const thankYouState = document.getElementById("thank-you-state");
+const submitBtn = document.getElementById("submit-btn");
+const ratingBtns = document.querySelector('.btn-rating');
+const scoreSpan = document.getElementById('rating-value');
+
+let selectedScore = null;
